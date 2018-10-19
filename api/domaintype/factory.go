a26d1bc35a64bf561e03bdb8f99f8b0e4de3b7ea
Jason Raimondi <jason@raimondi.us>
2018-10-19 10:40:40 -0700

    Updating the type factory to a more simple naming scheme.

--- api/domaintype/factory.go
+++ api/domaintype/factory.go
@@ -7,82 +7,178 @@ func NewDomainTypeFactory() *Factory {
 	return &Factory{}
 }
 
-func (f *Factory) GetAppVersionDomainModel() *AppVersion {
+func (f *Factory) Allotment() *Allotment {
+	return NewAllotment()
+}
+
+func (f *Factory) AppVersion() *AppVersion {
 	return NewAppVersion()
 }
 
-func (f *Factory) GetEmailDesignDomainModel() *EmailDesign {
+func (f *Factory) BugReport() *BugReport {
+	return NewBugReport()
+}
+
+func (f *Factory) Canvas() *Canvas {
+	return NewCanvas()
+}
+
+func (f *Factory) DomainMask() *DomainMask {
+	return NewDomainMask()
+}
+
+func (f *Factory) EmailDesign() *EmailDesign {
 	return NewEmailDesign()
 }
 
-func (f *Factory) GetEmailMessageDomainModel() *EmailMessage {
+func (f *Factory) EmailDesignType() *EmailDesignType {
+	return NewEmailDesignType()
+}
+
+func (f *Factory) EmailMessage() *EmailMessage {
 	return NewEmailMessage()
 }
 
-func (f *Factory) GetEmailTemplateDomainModel() *EmailTemplate {
+func (f *Factory) EmailNotification() *EmailNotification {
+	return NewEmailNotification()
+}
+
+func (f *Factory) EmailSample() *EmailSample {
+	return NewEmailSample()
+}
+
+func (f *Factory) EmailTemplate() *EmailTemplate {
 	return NewEmailTemplate()
 }
 
-func (f *Factory) GetEventDomainModel() *Event {
+func (f *Factory) Event() *Event {
 	return NewEvent()
 }
 
-func (f *Factory) GetImportDomainModel() *Import {
+func (f *Factory) Feature() *Feature {
+	return NewFeature()
+}
+
+func (f *Factory) FeatureToggle() *FeatureToggle {
+	return NewFeatureToggle()
+}
+
+func (f *Factory) Group() *Group {
+	return NewGroup()
+}
+
+func (f *Factory) Import() *Import {
 	return NewImport()
 }
 
-func (f *Factory) GetIntegrationDomainModel() *Integration {
+func (f *Factory) Integration() *Integration {
 	return NewIntegration()
 }
 
-func (f *Factory) GetIntegrationFieldMappingDomainModel() *IntegrationFieldMapping {
+func (f *Factory) IntegrationFieldMapping() *IntegrationFieldMapping {
 	return NewIntegrationFieldMapping()
 }
 
-func (f *Factory) GetIntegrationStatusMappingDomainModel() *IntegrationStatusMapping {
+func (f *Factory) IntegrationStatusMapping() *IntegrationStatusMapping {
 	return NewIntegrationStatusMapping()
 }
 
-func (f *Factory) GetInvitationDomainModel() *Invitation {
+func (f *Factory) Interaction() *Interaction {
+	return NewInteraction()
+}
+
+func (f *Factory) Invitation() *Invitation {
 	return NewInvitation()
 }
 
-func (f *Factory) GetPoolDomainModel() *Pool {
+func (f *Factory) OAuth() *OAuth {
+	return NewOAuth()
+}
+
+func (f *Factory) OAuth2() *OAuth2 {
+	return NewOAuth2()
+}
+
+func (f *Factory) Pool() *Pool {
 	return NewPool()
 }
 
-func (f *Factory) GetPromotionDomainModel() *Promotion {
+func (f *Factory) PoolContact() *PoolContact {
+	return NewPoolContact()
+}
+
+func (f *Factory) PoolContract() *PoolContract {
+	return NewPoolContract()
+}
+
+func (f *Factory) PoolFeature() *PoolFeature {
+	return NewPoolFeature()
+}
+
+func (f *Factory) Promotion() *Promotion {
 	return NewPromotion()
 }
 
-func (f *Factory) GetQueueDomainModel() *Queue {
+func (f *Factory) Queue() *Queue {
 	return NewQueue()
 }
 
-func (f *Factory) GetRegionDomainModel() *Region {
+func (f *Factory) Region() *Region {
 	return NewRegion()
 }
 
-func (f *Factory) GetReportDomainModel() *Report {
+func (f *Factory) Report() *Report {
 	return NewReport()
 }
 
-func (f *Factory) GetSalesforceDomainModel() *Salesforce {
+func (f *Factory) Salesforce() *Salesforce {
 	return NewSalesforce()
 }
 
-func (f *Factory) GetSalutationDomainModel() *Salutation {
+func (f *Factory) SalesforceEventSetting() *SalesforceEventSetting {
+	return NewSalesforceEventSetting()
+}
+
+func (f *Factory) SalesforcePoolSetting() *SalesforcePoolSetting {
+	return NewSalesforcePoolSetting()
+}
+
+func (f *Factory) Salutation() *Salutation {
 	return NewSalutation()
 }
 
-func (f *Factory) GetSitePageDomainModel() *SitePage {
+func (f *Factory) SitePage() *SitePage {
 	return NewSitePage()
 }
 
-func (f *Factory) GetStackDomainModel() *Stack {
+func (f *Factory) Stack() *Stack {
 	return NewStack()
 }
 
-func (f *Factory) GetUserDomainModel() *User {
+func (f *Factory) TicketBlock() *TicketBlock {
+	return NewTicketBlock()
+}
+
+func (f *Factory) TicketType() *TicketType {
+	return NewTicketType()
+}
+
+func (f *Factory) User() *User {
 	return NewUser()
+}
+
+func (f *Factory) UserAddress() *UserAddress {
+	return NewUserAddress()
+}
+
+func (f *Factory) UserAttribute() *UserAttribute {
+	return NewUserAttribute()
+}
+
+func (f *Factory) UserIdentifier() *UserIdentifier {
+	return NewUserIdentifier()
+}
+
+func (f *Factory) UserName() *UserName {
+	return NewUserName()
 }
