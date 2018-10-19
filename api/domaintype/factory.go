26e4922402a5ac810b271e9d791ab59c098b3b10
Jason Raimondi <jason@raimondi.us>
2018-10-19 11:45:25 -0700

    Fix undefined function issue. Do not generate factory functions for domain models without types.

--- api/domaintype/factory.go
+++ api/domaintype/factory.go
@@ -7,44 +7,16 @@ func NewDomainTypeFactory() *Factory {
 	return &Factory{}
 }
 
-func (f *Factory) Allotment() *Allotment {
-	return NewAllotment()
-}
-
 func (f *Factory) AppVersion() *AppVersion {
 	return NewAppVersion()
-}
-
-func (f *Factory) BugReport() *BugReport {
-	return NewBugReport()
-}
-
-func (f *Factory) Canvas() *Canvas {
-	return NewCanvas()
-}
-
-func (f *Factory) DomainMask() *DomainMask {
-	return NewDomainMask()
 }
 
 func (f *Factory) EmailDesign() *EmailDesign {
 	return NewEmailDesign()
 }
 
-func (f *Factory) EmailDesignType() *EmailDesignType {
-	return NewEmailDesignType()
-}
-
 func (f *Factory) EmailMessage() *EmailMessage {
 	return NewEmailMessage()
-}
-
-func (f *Factory) EmailNotification() *EmailNotification {
-	return NewEmailNotification()
-}
-
-func (f *Factory) EmailSample() *EmailSample {
-	return NewEmailSample()
 }
 
 func (f *Factory) EmailTemplate() *EmailTemplate {
@@ -53,18 +25,6 @@ func (f *Factory) EmailTemplate() *EmailTemplate {
 
 func (f *Factory) Event() *Event {
 	return NewEvent()
-}
-
-func (f *Factory) Feature() *Feature {
-	return NewFeature()
-}
-
-func (f *Factory) FeatureToggle() *FeatureToggle {
-	return NewFeatureToggle()
-}
-
-func (f *Factory) Group() *Group {
-	return NewGroup()
 }
 
 func (f *Factory) Import() *Import {
@@ -83,36 +43,12 @@ func (f *Factory) IntegrationStatusMapping() *IntegrationStatusMapping {
 	return NewIntegrationStatusMapping()
 }
 
-func (f *Factory) Interaction() *Interaction {
-	return NewInteraction()
-}
-
 func (f *Factory) Invitation() *Invitation {
 	return NewInvitation()
 }
 
-func (f *Factory) OAuth() *OAuth {
-	return NewOAuth()
-}
-
-func (f *Factory) OAuth2() *OAuth2 {
-	return NewOAuth2()
-}
-
 func (f *Factory) Pool() *Pool {
 	return NewPool()
-}
-
-func (f *Factory) PoolContact() *PoolContact {
-	return NewPoolContact()
-}
-
-func (f *Factory) PoolContract() *PoolContract {
-	return NewPoolContract()
-}
-
-func (f *Factory) PoolFeature() *PoolFeature {
-	return NewPoolFeature()
 }
 
 func (f *Factory) Promotion() *Promotion {
@@ -135,14 +71,6 @@ func (f *Factory) Salesforce() *Salesforce {
 	return NewSalesforce()
 }
 
-func (f *Factory) SalesforceEventSetting() *SalesforceEventSetting {
-	return NewSalesforceEventSetting()
-}
-
-func (f *Factory) SalesforcePoolSetting() *SalesforcePoolSetting {
-	return NewSalesforcePoolSetting()
-}
-
 func (f *Factory) Salutation() *Salutation {
 	return NewSalutation()
 }
@@ -155,30 +83,6 @@ func (f *Factory) Stack() *Stack {
 	return NewStack()
 }
 
-func (f *Factory) TicketBlock() *TicketBlock {
-	return NewTicketBlock()
-}
-
-func (f *Factory) TicketType() *TicketType {
-	return NewTicketType()
-}
-
 func (f *Factory) User() *User {
 	return NewUser()
-}
-
-func (f *Factory) UserAddress() *UserAddress {
-	return NewUserAddress()
-}
-
-func (f *Factory) UserAttribute() *UserAttribute {
-	return NewUserAttribute()
-}
-
-func (f *Factory) UserIdentifier() *UserIdentifier {
-	return NewUserIdentifier()
-}
-
-func (f *Factory) UserName() *UserName {
-	return NewUserName()
 }
